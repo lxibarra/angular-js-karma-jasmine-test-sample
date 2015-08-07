56ce670dc63a306c2b34469da5259cfa3b9d5031
Ricardo Ibarra <ribarra@tekmexico.com>
2015-08-07 14:09:25 -0700

    mockikng ajax request done

---
+++ tests/karma-validation.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('checking karma configuration', function() {
+
+    it('Karma is working', function() {
+       expect(1).toBe(1);
+    });
+
+});
---
+++ tests/simple.assert.spec.js
@@ -0,0 +1,19 @@
+describe('first test', function() {
+    var counter = 0;
+
+    beforeEach(function() {
+        counter = 0;
+    });
+
+    it('Increments value', function() {
+        counter++;
+        expect(counter).toBe(1);
+    });
+
+    it('Decrease value', function() {
+        counter--;
+        expect(counter).toBe(-1);
+    });
+
+
+});
