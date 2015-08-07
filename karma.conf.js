fe6708cbd2d1426b5ffe75c0842cd1b36721d3ac
Ricardo Ibarra <coder10@gmail.com>
2015-08-07 16:01:29 -0700

    Added karma plugins

--- karma.conf.js
+++ karma.conf.js
@@ -38,6 +38,11 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
+    plugins : [
+      'karma-phantomjs-launcher',
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ],
 
     // web server port
     port: 9876,
